6bbc91a2129fee3775eaff49da99cfef7bb4531f
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2023-11-06 01:36:47 +0600

    Starting game blog project

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+        <nav>
+            <p>Home</p>
+            <p>Create Blog</p>
+            <p>Games</p>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
