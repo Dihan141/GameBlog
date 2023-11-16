c374d9101584edf5e71a6be7a0ae4e0210f8ee86
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2023-11-17 02:08:05 +0600

    jwt added

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../Models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (user) => {
+    return jwt.sign({user}, process.env.JWT_SECRET, {expiresIn: '10d'})
+}
 
 const getUsers = async (req, res) => {
     res.status(200).json({msg: "Get all users"})
@@ -8,7 +13,8 @@ const createNewUser = async (req, res) => {
     try{
         const user = await User.signUp(req.body)
 
-        res.status(200).json(user)
+        const token = createToken(user._id)
+        res.status(200).json({user,token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
